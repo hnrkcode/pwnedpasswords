4c93858df44d67569dc55dff97f1996d30e7d06c
Henrik Petersson <henrik@tutamail.com>
2019-03-21 20:56:31 +0100

    Refactored the code to OOP

--- pwnedpasswords.py
+++ pwnedpasswords.py
@@ -7,56 +7,60 @@ import requests
 import argparse
 
 
-def hashify(password):
-    """Hash function that returns a uppercase hash value of the password."""
-    hash = hashlib.sha1()
-    hash.update(bytes(password, encoding="utf-8"))
-    hash = hash.hexdigest().upper()
-    return hash
+class CheckPassword:
 
+    def __init__(self):
+        self.url = "https://api.pwnedpasswords.com/range/"
 
-def check_password(hash):
-    """Get all hashes in the database that matches the five first characters
-    in the passwords hash value.
-    Then check how many times the password occurred in the databases."""
-    # Get hashes that match the five first characters and save them to a file.
-    prefix, suffix = hash[:5], hash[5:]
-    try:
-        r = requests.get(f"https://api.pwnedpasswords.com/range/{prefix}")
-    except requests.exceptions.ConnectionError as e:
-        sys.exit("Connection error.")
-    else:
+    def _hashify(self, password):
+        """Returns an uppercase hash value of the password."""
+        hash = hashlib.sha1()
+        hash.update(bytes(password, encoding="utf-8"))
+        return hash.hexdigest().upper()
+
+    def _temp_save(self, hashes):
+        """Temporary store the fetched hashes in a file."""
         with open("hashes.txt", "w") as output:
-            output.write(r.text)
-        # Locally count how many of the hashes that matches the passwords hash.
+            output.write(hashes)
+
+    def _count(self, suffix):
+        """Locally count the hashes that matches the passwords hash."""
+        occur = 0
         with open("hashes.txt") as f:
             for line in f.readlines():
                 if line[:35] == suffix:
-                    occurrence = int(line[36:])
-                    os.remove("hashes.txt")
-                    return occurrence
-            os.remove("hashes.txt")
-            return 0
+                    occur = int(line[36:])
+        os.remove("hashes.txt")
+        return occur
+
+    def check(self, password):
+        """Hash password and check it against hashes of leaked passwords."""
+        password_hash = self._hashify(password)
+        prefix, suffix = password_hash[:5], password_hash[5:]
+        try:
+            fetched_hashes = requests.get(f"{self.url}{prefix}")
+        except requests.exceptions.ConnectionError as e:
+            sys.exit("Connection error.")
+        else:
+            # Temporary store the fetched hashes in a file.
+            self._temp_save(fetched_hashes.text)
+            # Locally count the hashes that matches the passwords hash.
+            occur = self._count(suffix)
+            return occur
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("password", help=("Password that will be hashed and" +
-                        " checked locally against hashes of leaked" +
-                        " passwords on haveibeenpwned.com")
-                        )
+    parser.add_argument("password", help="Password to hash and check.")
     args = parser.parse_args()
-    hash = hashify(args.password)
-    occurrence = check_password(hash)
+    haveibeenpwned = CheckPassword()
+    occur = haveibeenpwned.check(args.password)
 
-    if occurrence == 1:
-        print(f"Your password occurred {occurrence} time in the database.")
-        print("You should change your password.")
-    elif occurrence > 1:
-        print(f"Your password occurred {occurrence} times in the database.")
-        print("You should change your password.")
-    elif occurrence == 0:
-        print(f"Your password was not in the database.")
+    if occur > 0:
+        numerus = lambda num: "time." if num == 1 else "times!"
+        print(f"\"{args.password}\" have been pwned {occur} {numerus(occur)}")
+    elif occur == 0:
+        print(f"\"{args.password}\" doesn't appear to be pwned.")
 
 
 if __name__ == "__main__":
