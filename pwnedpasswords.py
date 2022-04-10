c69bc843b14eff6af685650c81586e3a95985950
Henrik Petersson <44243358+hnrkcode@users.noreply.github.com>
2022-04-10 16:24:24 +0200

    Make password check asynchronous to speed up csv feature

--- pwnedpasswords.py
+++ pwnedpasswords.py
@@ -1,11 +1,11 @@
 import argparse
+import asyncio
 import csv
 import hashlib
 import sys
 from collections import defaultdict
 
-import requests
-
+import aiohttp
 
 API_URL = "https://api.pwnedpasswords.com/range/"
 
@@ -21,7 +21,7 @@ def _hashify(password):
 def _get_matching_hash_count(suffix, data):
     """Locally count the hashes that matches the passwords hash."""
     password_leak_count = 0
-    lines = data.text.split("\n")
+    lines = data.split("\n")
 
     for line in lines:
         if line[:35] == suffix:
@@ -30,65 +30,67 @@ def _get_matching_hash_count(suffix, data):
     return password_leak_count
 
 
-def _message(password, password_leak_count):
+def _message(name, password, password_leak_count):
     """Print the result to the screen."""
     if password_leak_count:
         numerus = lambda num: "time." if num == 1 else "times!"
         print(
-            f'"{password}" have been pwned {password_leak_count} {numerus(password_leak_count)}'
+            f'"{name.upper()}: {password}" have been pwned {password_leak_count} {numerus(password_leak_count)}'
         )
     else:
-        print(f'No match for "{password}".')
+        print(f'{name.upper()}: No match for "{password}".')
 
 
-def check_password(password, msg=False):
-    """Hash password and check it against hashes of leaked passwords."""
-    password_hash = _hashify(password)
-    prefix, suffix = password_hash[:5], password_hash[5:]
+async def check_password(name, work_queue):
+    async with aiohttp.ClientSession() as session:
+        while not work_queue.empty():
+            password = await work_queue.get()
 
-    try:
-        fetched_hashes = requests.get(f"{API_URL}{prefix}")
-    except requests.exceptions.ConnectionError:
-        sys.exit("Connection error.")
+            password_hash = _hashify(password)
+            prefix, suffix = password_hash[:5], password_hash[5:]
 
-    password_leak_count = _get_matching_hash_count(suffix, fetched_hashes)
-
-    if msg:
-        _message(password, password_leak_count)
-
-    return password_leak_count
+            async with session.get(f"{API_URL}{prefix}") as response:
+                fetched_hashes = await response.text()
+                password_leak_count = _get_matching_hash_count(suffix, fetched_hashes)
+                _message(name, password, password_leak_count)
 
 
-def check_csv(file):
-    column = defaultdict(list)
-
-    try:
-        with open(file) as csvfile:
-            csvreader = csv.DictReader(csvfile)
-            for row in csvreader:
-                for key, value in row.items():
-                    column[key].append(value)
-    except FileNotFoundError as e:
-        sys.exit(e)
-
-    for password in column["password"]:
-        password_leak_count = check_password(password)
-        _message(password, password_leak_count)
-
-
-def main():
+async def main():
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-p", "--password", help="Check single password.")
     group.add_argument("-f", "--csvfile", help="Check passwords in CSV-file.")
     args = parser.parse_args()
 
+    passwords = []
+
     if args.password:
-        check_password(args.password, msg=True)
+        passwords.append(args.password)
 
     if args.csvfile:
-        check_csv(args.csvfile)
+        column = defaultdict(list)
+
+        try:
+            with open(args.csvfile) as csvfile:
+                csvreader = csv.DictReader(csvfile)
+                for row in csvreader:
+                    for key, value in row.items():
+                        column[key].append(value)
+        except FileNotFoundError as e:
+            sys.exit(e)
+
+        for password in column["password"]:
+            passwords.append(password)
+
+    work_queue = asyncio.Queue()
+
+    for password in passwords:
+        await work_queue.put(password)
+
+    await asyncio.gather(
+        *[asyncio.create_task(check_password(str(i), work_queue)) for i in range(10)],
+    )
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
